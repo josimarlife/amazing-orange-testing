ce03ff23f7b65c84ba54426ee0377847ef4f63ba
josimarlife <josimar@olelife.com>
2024-04-09 01:18:37 -0500

    ✨ Modified dirname of html reports
    Modified dirname of html reports

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,7 +1,9 @@
 const report = require("multiple-cucumber-html-reporter");
+const path = require("path");
 report.generate({
   jsonDir: "jsonlogs", // ** Path of .json file **//
-  reportPath: "./reports/cucumber-htmlreport.html",
+  //reportPath: "./reports/cucumber-htmlreport.html",
+  reportPath: path.join(__dirname, "reports", "cucumber-htmlreport.html"),
   metadata: {
     browser: {
       name: "chrome",
